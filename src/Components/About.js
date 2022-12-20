e161a2d734c68af179c9bc37531f3b955490b7a9
Musyoki <vincenbryaniel@gmail.com>
2022-12-20 13:20:14 +0300

    updated about page

--- src/Components/About.js
+++ src/Components/About.js
@@ -6,7 +6,7 @@ class About extends Component {
     if (!this.props.data) return null;
 
     const name = this.props.data.name;
-    const profilepic = "images/" + this.props.data.image;
+    // const profilepic = "images/" + this.props.data.image;
     const bio = this.props.data.bio;
     const street = this.props.data.address.street;
     const city = this.props.data.address.city;
@@ -21,11 +21,11 @@ class About extends Component {
         <Fade duration={1000}>
           <div className="row">
             <div className="three columns">
-              <img
+              {/* <img
                 className="profile-pic"
                 src={profilepic}
                 alt="Nordic Giant Profile Pic"
-              />
+              /> */}
             </div>
             <div className="nine columns main-col">
               <h2>About Me</h2>
